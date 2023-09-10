31dce5713f1e5433979e7c9d1910d398364440a5
andyh444 <47712158+andyh444@users.noreply.github.com>
2023-09-10 21:28:16 +0100

    Create IngredientEditor control

---
+++ frontend/recipe-manager/src/components/IngredientEditor.tsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+import { IngredientProperties } from "../../../../common/sharedtypes/IngredientProperties";
+
+export interface IIngredientEditorProps {
+    onFinish: (ingredient:IngredientProperties) => void;
+}
+
+export function IngredientEditor (props: IIngredientEditorProps) {
+    const ingredientEntryRef = useRef<HTMLInputElement>(null);
+    const shelfLifeRef = useRef<HTMLInputElement>(null);
+    const unitRef = useRef<HTMLInputElement>(null);
+
+    let getIngredient = (): IngredientProperties => {
+        const ingredientProperties:IngredientProperties = {
+            name: ingredientEntryRef.current?.value ?? "",
+            unit: unitRef.current?.value ?? "",
+            shelfLifeDays: Number(shelfLifeRef.current?.value ?? 1)
+            };
+        return ingredientProperties
+    }
+
+    return (
+        <div>
+            <span>
+                <input ref={ingredientEntryRef} placeholder='Enter an ingredient'></input>
+                <input ref={shelfLifeRef} type='number' min={ 1 } placeholder='Enter a shelf life (Days)'></input>
+                <input ref={unitRef} placeholder='Enter a unit (gram, millilitre, single)'></input>
+                <button onClick={() => props.onFinish(getIngredient())}>Add</button>
+            </span>
+        </div>  
+    );
+}
--- frontend/recipe-manager/src/components/IngredientsPage.tsx
+++ frontend/recipe-manager/src/components/IngredientsPage.tsx
@@ -3,6 +3,7 @@ import Select from 'react-select';
 import { Ingredient } from '../../../../common/sharedtypes/Ingredient';
 import { IngredientControl } from './IngredientControl';
 import { IngredientProperties } from '../../../../common/sharedtypes/IngredientProperties';
+import { IngredientEditor } from './IngredientEditor';
 
 interface IngredientsPageProps {
     ingredients: Ingredient[];
@@ -15,28 +16,13 @@ const IngredientsPage: React.FC<IngredientsPageProps> = (props) => {
   const shelfLifeRef = useRef<HTMLInputElement>(null);
   const unitRef = useRef<HTMLInputElement>(null);
 
-  const addIngredient = () => {
-    const ingredientProperties:IngredientProperties = {
-      name: ingredientEntryRef.current?.value ?? "",
-      unit: unitRef.current?.value ?? "",
-      shelfLifeDays: Number(shelfLifeRef.current?.value ?? 1)
-    };
-    console.log("adding ingredient", ingredientProperties);
-    props.onAddIngredient(ingredientProperties);
-  }
-
   return  (
     <div>
         <h1>Ingredients</h1>
-        <span>
-            <input ref={ingredientEntryRef} placeholder='Enter an ingredient'></input>
-            <input ref={shelfLifeRef} type='number' min={ 1 } placeholder='Enter a shelf life (Days)'></input>
-            <input ref={unitRef} placeholder='Enter a unit (gram, millilitre, single)'></input>
-            <button onClick={addIngredient}>Add</button>
-        </span>
-            {props.ingredients.map((ingredient) => (
-            <IngredientControl key={ingredient.id} ingredient={ingredient} onDeleteIngredient={props.onDeleteIngredient} />
-            ))}
+        <IngredientEditor onFinish={i => props.onAddIngredient(i)} />
+        {props.ingredients.map((ingredient) => (
+        <IngredientControl key={ingredient.id} ingredient={ingredient} onDeleteIngredient={props.onDeleteIngredient} />
+        ))}
     </div>
   );
 };
