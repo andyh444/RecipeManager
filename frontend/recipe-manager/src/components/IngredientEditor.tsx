6275fb2824bf0413ca6a1ed626d345033929294d
andyh444 <47712158+andyh444@users.noreply.github.com>
2023-09-14 18:20:01 +0100

    add field validation to ingredient editor

--- frontend/recipe-manager/src/components/IngredientEditor.tsx
+++ frontend/recipe-manager/src/components/IngredientEditor.tsx
@@ -12,6 +12,16 @@ export function IngredientEditor (props: IIngredientEditorProps) {
     const shelfLifeRef = useRef<HTMLInputElement>(null);
     const unitRef = useRef<HTMLSelectElement>(null);
 
+    let validateFields = (): boolean => {
+        // javascript is a stupid language
+        if (ingredientEntryRef.current?.value
+            && shelfLifeRef.current?.value
+            && unitRef.current?.value) {
+                return true;
+        }
+        return false;
+    }
+
     let getIngredient = (): IngredientProperties => {
         const ingredientProperties:IngredientProperties = {
             name: ingredientEntryRef.current?.value ?? "",
@@ -20,6 +30,18 @@ export function IngredientEditor (props: IIngredientEditorProps) {
             shelfLifeDays: Number(shelfLifeRef.current?.value ?? 1)
             };
         return ingredientProperties
+    }
+
+    let resetFields = (): void => {
+        if (ingredientEntryRef.current !== null) {
+            ingredientEntryRef.current.value = "";
+        }
+        if (shelfLifeRef.current !== null) {
+            shelfLifeRef.current.value = "";
+        }
+        if (unitRef.current !== null) {
+            unitRef.current.value = "";
+        }
     }
 
     return (
@@ -33,7 +55,15 @@ export function IngredientEditor (props: IIngredientEditorProps) {
                     <option>Millilitre</option>
                     <option>Single</option>
                 </select>
-                <button onClick={() => props.onFinish(getIngredient())}>{props.buttonText}</button>
+                <button onClick={() => {
+                    if (validateFields()) {
+                        props.onFinish(getIngredient())
+                        resetFields()
+                    } else {
+                        // TODO: More user friendly message
+                        alert("Invalid fields!!!")
+                    }
+                    }}>{props.buttonText}</button>
             </span>
         </div>  
     );
